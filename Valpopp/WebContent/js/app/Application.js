ed75900e8f47e1d20e3fc6f3cfc40f2e379a9904
Gaston Cocce <gastoncocce@hotmail.com>
2013-09-26 19:21:08 +0200

    Inicial Structure with lib files.

--- Valpopp/WebContent/js/app/Application.js
+++ Valpopp/WebContent/js/app/Application.js
@@ -1,4 +1,62 @@
 
+
+window.addEventListener('load', eventWindowLoaded, false);
+
+function eventWindowLoaded() {
+   init();
+}
+
+var scenario = null;
+var scenario_schema=null;
+var scenario_obj=null;
+
+function init(){
+   console.log("Page Already loaded.");
+
+   // Load schema
+   jQuery.getJSON('schema_demo.json', function(data) {
+      scenario_schema=JSON.stringify(data);
+      console.log("Scenario Schema");
+      console.log(scenario_schema);
+   })
+   .error(function() {
+      alert("getJSON ERROR. SCHEMA.JSON is not valid json.");
+   });
+   
+   scenario = new Scenario();   
+}
+   
+function validate() {
+   console.log("Se ejecuta validate");
+   
+   var result= document.getElementById('result');
+   result.innerHTML = "";    
+   
+   var elfile=document.getElementById('file');
+
+   if (!scenario.openFile(elfile)) {
+      var error=scenario.getError();
+      alert(error);    
+   }  
+}
+
+function contenido() {
+   console.log("Se ejecuta contenido");
+      
+   var result= document.getElementById('result');
+   
+   if (scenario.isValid()) {
+      result.innerHTML = scenario.getContents();
+   }else{
+      //result.innerHTML = scenario.getError();
+      result.innerHTML = scenario.getContents();
+   }
+}
+
+
+//***************************************************************************************
+
+
 alert("I am working");
 
 function Car( model, year, miles ) {
---
+++ Valpopp/WebContent/js/utils/FileHandler.js
@@ -0,0 +1,94 @@
+// This classe is intended to manipulate File, FileReader, FileList and Blob Apis
+// and to provide a secure way to manipulate local files
+
+
+function FileHandler() {
+    var m_enabled=false;
+    var m_codification="UTF-8";
+    var m_error=null;
+    
+    var m_contents=null;
+    var m_type=null;
+    var m_size=null;
+    var m_name=null;
+    
+    this.isEnabled=isEnabled;
+    this.setCodification=setCodification;
+    this.openFile=openFile;
+    this.getName=getName;
+    this.getType=getType;
+    this.setType=setType;
+    this.getSize=getSize;
+    this.getError=getError;
+      
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        m_enabled=true;
+    }else{
+        console.error("File FileReader FileList Blob not Enabled!");
+    }
+    
+    function isEnabled() {
+        return m_enabled;
+    }
+    
+    function setCodification(arg) {
+        m_codification=arg;
+    }
+
+    /* The parameter must be an input file html element */
+    /* Returns true or false according to result */
+    function openFile(arg, readCallback) {
+        // Get File Object
+        var fHandler = arg.files[0];
+        
+        if (fHandler) {
+            try {
+                m_type=fHandler.type;
+                m_size=fHandler.size;
+                m_name=fHandler.name;
+                
+               var fReader = new FileReader();
+               
+               fReader.onerror = function (e){
+                    console.log("Error code %s", e.target.error.code);
+                    m_error=e.target.error;
+               }
+                
+                fReader.onload = readCallback;
+                    
+                fReader.readAsText(fHandler, m_codification);
+                
+                console.log("FileHandler, file type: "+ fHandler.type);
+            } catch(e) {
+                console.error(e);
+                alert(e);
+                return false;
+            }
+            return true;
+        } else {
+            m_error="It seems that there is no selected file!";
+            console.error(m_error);
+            return false;
+        }         
+    }
+    
+    function getName() {
+        return m_name;
+    }
+    
+    function getType() {
+        return m_type;
+    }
+    
+    function setType(arg) {
+        m_type=arg;
+    }
+    
+    function getSize() {
+        return m_size;
+    }
+   
+    function getError() {
+        return m_error;
+    }
+}
