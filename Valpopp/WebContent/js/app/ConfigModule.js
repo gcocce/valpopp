51fd6a08e2fe7d53d27b84108f8f171b3d6f5573
Gaston Cocce <gastoncocce@hotmail.com>
2013-10-01 15:59:07 +0200

    Basic functionality of config and language modules.

--- Valpopp/WebContent/js/app/ConfigModule.js
+++ Valpopp/WebContent/js/app/ConfigModule.js
@@ -1,42 +1,35 @@
-var myModule = {
 
-  myProperty: "someValue",
 
-  // object literals can contain properties and methods.
-  // e.g we can define a further object for module configuration:
-  myConfig: {
-    useCaching: true,
-    language: "en"
-  },
+var configModule = {
+		// Config properties
+		lang:"en",
+		listIdLang: ["en", "es"],
+		listCaptionLang: ["English", "Espanish"],
+		
+		user:"basic",
+		
+		getLanguages:function(){
+			return this.listIdLang;
+		},
+  
+		getLang: function () {
+			console.log("configModule.getLang()");
+			return this.lang;
+		},
+  
+		setLang: function (lang){
+			console.log("configModule.setLang()");
+			this.lang=lang;	  
+		},
 
-  // a very basic method
-  myMethod: function () {
-    console.log( "Where in the world is Paul Irish today?" );
-  },
-
-  // output a value based on the current configuration
-  myMethod2: function () {
-    console.log( "Caching is:" + ( this.myConfig.useCaching ) ? "enabled" : "disabled" );
-  },
-
-  // override the current configuration
-  myMethod3: function( newConfig ) {
-
-    if ( typeof newConfig === "object" ) {
-      this.myConfig = newConfig;
-      console.log( this.myConfig.language );
-    }
-  }
+		getUserMode: function(  ) {
+			console.log("configModule.getUserMode()");
+			return this.user;
+		 },
+		 
+		// override the current configuration
+		setUserMode: function(user) {
+			console.log("configModule.setUserMode()");
+			this.user=user;
+		 }		 
 };
-
-// Outputs: Where in the world is Paul Irish today?
-myModule.myMethod();
-
-// Outputs: enabled
-myModule.myMethod2();
-
-// Outputs: fr
-myModule.myMethod3({
-  language: "fr",
-  useCaching: false
-});
--- Valpopp/WebContent/strings/LanguageModule.js
+++ Valpopp/WebContent/strings/LanguageModule.js
@@ -1,43 +1,99 @@
-var basketModule = (function () {
 
-  // Privates
-  var basket = []; 
+var languageModule = (function () {
+	// Privates
+	var numLanguages=0;
+	var langArr=new Array();
+	var m_error="";
+	var fileContents="";
+	
+	// List of available languages
+	var languagesId=configModule.getLanguages();
+	//var languagesCaption=configModule.get;
+	
+	// Default Language
+	var currentlang = configModule.getLang();
 
-  function doSomethingPrivate() {
-    //...
-  }
+	
+	var consts = new Array();
+	var jqxhr=$.get('strings/language.csv', function(data){
+		fileContents=data;
+		consts = data.split('\n');
+		var firstline=consts[0];
+		langArr=firstline.split(";");
+		langArr.shift();
+		console.log(langArr);
+		consts.shift();
+		console.log(consts);
+		m_error="File readed";
 
-  function doSomethingElsePrivate() {
-    //...
-  }
+		// Create a new jQuery.Event object without the "new" operator.
+		var eventLanguageFile = $.Event( "LanguageFileLoaded" );
+		// Dispatch the event
+		$( "body" ).trigger( eventLanguageFile );
+	});	
+	
+	// Set another completion function for the request above
+	jqxhr.fail(function() {
+	  m_error="Error reading file!";
+	});	
+
+	
+	// Object to hold all the captions
+	var captions= {};
+	
+	// Variable to hold all the contents of each language file
+	var filesContent=new Array();
+	
+	
+	function doInitialization() {
+		console.log("languageModule.Initialize()");
+		
+		if (m_error.localeCompare("")==0){
+			m_error="Initialized";	
+		}
+			
+		
+		for (var c=0; c < consts.length; c++){
+			var tempc=consts;
+			var line=tempc[c].split(";");
+			
+			// Do not consider blank lines
+			if (line.length > 1){
+				captions[line[0]]={};
+			}
+		}	
+		
+		console.log(captions);
+		
+		for (var c=0; c < consts.length; c++){
+			var line=consts[c].split(";");
+			
+			// Do not consider blank lines
+			if (line.length > 1){
+				for (var l=1; l<line.length; l++){
+					captions[line[0]][langArr[l-1]]=line[l];
+					console.log(captions[line[0]][langArr[l-1]]);
+				}
+			}
+		}
+		
+		console.log(captions);
+	}
+
 
   // Return an object exposed to the public
-  return { 
-
-    // Add items to our basket
-    addItem: function( values ) {
-      basket.push(values);
-    },
-
-    // Get the count of items in the basket
-    getItemCount: function () {
-      return basket.length;
-    },
-
-    // Public alias to a  private function
-    doSomething: doSomethingPrivate,
-
-    // Get the total value of items in the basket
-    getTotal: function () {
-
-      var q = this.getItemCount(),
-          p = 0;
-
-      while (q--) {
-        p += basket[q].price;
-      }
-
-      return p;
-    }
-  };
+	return { 
+		getCaption: function (caption){
+			console.log("getCaption for: "+ caption + " lang:"+currentlang);
+			return captions[caption][currentlang];
+		},
+		initialize: doInitialization,
+		getError: function (){
+			return m_error;
+		},
+		getFileContents: function (){
+			return fileContents;
+		}
+		
+	};
 }());
